a39b89cc940e70baff0d52b27f02e6cfa9056267
Nick Mazzotta <nickamazzotta@gmail.com>
2018-03-07 12:05:38 -0500

    initialized spring project with Web DevTools JPA and H2

---
+++ src/main/java/com/simpleRestfulWebServices/foodservices/FoodServicesApplication.java
@@ -0,0 +1,12 @@
+package com.simpleRestfulWebServices.foodservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodServicesApplication.class, args);
+	}
+}
